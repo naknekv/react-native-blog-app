2dff2bea7529beabb2e8ee3e7f74728b21050045
Ignacio Iacovino <nacho.iacovino@gmail.com>
2020-01-06 02:10:06 +0100

    Add ability to remove posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,18 +1,40 @@
-import React, { createContext } from 'react'
+import React, { createContext,  useState, useReducer } from 'react'
+import uuid from 'uuid/v4'
+import createDataContext from './createDataContext'
 
-const BlogContext = createContext()
+// const BlogContext = createContext()
 
-export const BlogProvider = ({ children }) => {
-    const blogPosts = [
-        { title: "Blog post #1" },
-        { title: "Blog post #2" }
-    ]
+const initialState = [
+    { title: "Blog post #1", id: uuid() },
+    { title: "Blog post #2", id: uuid()  }
+]
 
-    return (
-        <BlogContext.Provider value={blogPosts}>
-            {children}
-        </BlogContext.Provider>
-    )
+const reducer = (state, { type, payload }) => {
+    switch (type) {
+        case "create":
+            return [...state, { title: payload, id: uuid() }] 
+        case "destroy":
+            return state.filter(({ id }) => id !== payload)
+        default:
+            return state
+    }
 }
 
-export default BlogContext
+// const createPost = dispatch => dispatch({ type: 'create' })
+
+export const { Context: BlogContext, Provider: BlogProvider } = createDataContext(reducer, initialState)
+
+
+// export const BlogProvider = ({ children }) => {
+//     const [state, dispatch] = useReducer(reducer, initialState)
+
+//     // const addBlogPost = title => setBlogPosts([...blogPosts, { title, id: uuid() }])
+
+//     return (
+//         <BlogContext.Provider value={{ state, dispatch }}>
+//             {children}
+//         </BlogContext.Provider>
+//     )
+// }
+
+// export default BlogContext
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,18 +1,46 @@
 import React, { useContext } from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
-import BlogContext from '../context/BlogContext'
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons'
+import { BlogContext } from '../context/BlogContext'
 
 const IndexScreen = () => {
-    const blogPosts = useContext(BlogContext)
+    const { state: blogPosts, dispatch: crudBlogPosts } = useContext(BlogContext)
     return (
         <View>
+            <Button title="Add Post" onPress={() => crudBlogPosts({ type: "create", payload: "My new post!"})} />
             <FlatList 
                 data={blogPosts}
-                keyExtractor={({ title }) => title} 
-                renderItem={({ item }) => <Text>{item.title}</Text>}
+                keyExtractor={({ id }) => id} 
+                renderItem={({ item }) => 
+                    <View style={styles.rowStyle}>
+                        <Text style={styles.titleStyle}>{item.title}</Text>
+                        <TouchableOpacity onPress={() => crudBlogPosts({ type: "destroy", payload: item.id})}>
+                            <Feather name="trash" style={styles.iconStyle} />
+                        </TouchableOpacity>
+                    </View>
+                }
             />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    rowStyle: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        borderBottomWidth: 1,
+        borderColor: "gray"
+    },
+    titleStyle: {
+        fontSize: 18,
+    },
+    iconStyle: {
+        fontSize: 24
+    }
+})
+
 
 export default IndexScreen
